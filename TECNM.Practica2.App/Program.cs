8ff7aaeff4d66d71b9422df58103a7c5d641bc89
Gaby-Garcia <garciagaby839@gmail.com>
2024-02-11 19:03:29 -0600

    Calculadora de peso en Marte

---
+++ TECNM.Practica2.App/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using TECNM.Practica2.Core.Entities;
+using TECNM.Practica2.Core.Managers;
+using TECNM.Practica2.Core.Services;
+
+namespace TECNM.Practica2.App;
+
+public static class Program{
+    public static void Main(String[] args ){
+
+        float weight = 0;
+
+        System.Console.Write("Ingresa tu peso en Kg: ");
+        Single.TryParse(System.Console.ReadLine(), out weight);
+
+        //Instancia a clase Persona y Newton-weight
+        var person = new Person{Weight = weight};
+
+        var service = new NwService();
+        var managers = new NwManager(service);
+
+        Nw nw = managers.GetNw(person);
+
+        System.Console.WriteLine($"Tu peso en Marte es de: {nw.Index} Kg");
+    }
+}
